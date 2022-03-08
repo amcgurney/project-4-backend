d355cff5797303d07e71a06f8da8b2ba9fa8d80b
Alan McGurney <mcgurneyalan@gmail.com>
2022-03-08 16:33:13 -0500

    2nd commits

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-///////////////////////////////
+
 // DEPENDENCIES
-////////////////////////////////
+
 // get .env variables
 require("dotenv").config();
 // pull PORT from .env, give default value of 3000
@@ -12,10 +12,13 @@ const express = require("express");
 const app = express();
 // import mongoose
 const mongoose = require("mongoose");
+// import middleware
+const cors = require("cors");
+const morgan = require("morgan");
 
-///////////////////////////////
-// DATABASE CONNECTION
-////////////////////////////////
+
+// DATABASE CONNECTIONS
+
 // Establish Connection
 mongoose.connect(MONGODB_URL, {
   useUnifiedTopology: true,
@@ -27,15 +30,80 @@ mongoose.connection
   .on("close", () => console.log("Your are disconnected from mongoose"))
   .on("error", (error) => console.log(error));
 
-///////////////////////////////
+// MODELS
+
+const UserSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    username: String,
+    password: String,
+  });
+
+const User = mongoose.model("User", UserSchema);
+
+// MiddleWare
+
+app.use(cors()); // to prevent cors errors, open access to all origins
+app.use(morgan("dev")); // logging
+app.use(express.json()); // parse json bodies
+
+
+
+
 // ROUTES
-////////////////////////////////
+
 // create a test route
 app.get("/", (req, res) => {
   res.send("Musician's Friend");
 });
 
-///////////////////////////////
+// USER INDEX ROUTE
+app.get("/user", async (req, res) => {
+    try {
+      // send all users
+        res.json(await User.find({}));
+    } catch (error) {
+      //send error
+        res.status(400).json(error);
+    }
+});
+
+  // USER CREATE ROUTE
+app.post("/user", async (req, res) => {
+    try {
+      // send all users
+        res.json(await User.create(req.body));
+    } catch (error) {
+      //send error
+        res.status(400).json(error);
+    }
+});
+
+// USER UPDATE ROUTE
+app.put("/user/:id", async (req, res) => {
+    try {
+      // send all users
+      res.json(
+        await User.findByIdAndUpdate(req.params.id, req.body, { new: true })
+      );
+    } catch (error) {
+      //send error
+      res.status(400).json(error);
+    }
+  });
+  
+  // USER DELETE ROUTE
+  app.delete("/user/:id", async (req, res) => {
+    try {
+      // send all users
+      res.json(await People.findByIdAndRemove(req.params.id));
+    } catch (error) {
+      //send error
+      res.status(400).json(error);
+    }
+  });
+
+
 // LISTENER
-////////////////////////////////
+
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
