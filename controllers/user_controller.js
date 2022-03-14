f5598efaebd49149c02ad33b1e4f0fb61b88949a
Alan McGurney <mcgurneyalan@gmail.com>
2022-03-14 17:53:12 -0400

    heroku attempt1

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { User } = require("../models");
 
+
 router.get("/", async (req, res) => {
     try {
         res.json(await User.find({}));
